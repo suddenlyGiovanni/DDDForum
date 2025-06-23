29cd60f8b55db13a0770f9999fa8f9a9c7344ea7
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-06-23 21:39:19 +0200

    refactor: simplify user route definitions and update HTTP examples
    - Removed `/new` from `POST` route in `users.controller.ts` for streamlined endpoint naming.
    - Adjusted corresponding HTTP examples in `users.http` to reflect updated route structure.
    - Enhanced code readability with consistent formatting in `users.controller.ts`.

--- backend/src/controllers/users/users.controller.ts
+++ backend/src/controllers/users/users.controller.ts
@@ -2,8 +2,8 @@ import type { DatabaseSync } from 'node:sqlite'
 
 import express, { type Router } from 'express'
 
-import { UserRepository }     from '../../models/user/user.repository.ts'
-import { makeCreateUser }     from './create-user.handler.ts'
+import { UserRepository } from '../../models/user/user.repository.ts'
+import { makeCreateUser } from './create-user.handler.ts'
 import { makeEditUser } from './edit-user.handler.ts'
 import { makeGetUserByEmail } from './get-user-by-email.handler.ts'
 
@@ -11,8 +11,8 @@ export const makeUsersRouter = (database: DatabaseSync): Router => {
 	const userRepo = UserRepository.make(database)
 
 	return express
-	.Router()
-	.get('/', makeGetUserByEmail(userRepo))
-	.post('/new', makeCreateUser(userRepo))
-	.put('/edit/:userId', makeEditUser(userRepo))
+		.Router()
+		.get('/', makeGetUserByEmail(userRepo))
+		.post('/', makeCreateUser(userRepo))
+		.put('/edit/:userId', makeEditUser(userRepo))
 }
