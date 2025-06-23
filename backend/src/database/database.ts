85e7b0826fc41796e09271e42782ee24a0217e61
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-06-23 18:43:15 +0200

    feat: improve database initialization and error handling
    - Introduced `InitializationError` and `DatabaseError` for more specific error handling in the database module.
    - Enhanced `initializeSchema` and `openDatabaseSync` functions with robust try-catch blocks to handle initialization and opening errors gracefully.
    - Added proper resource cleanup by closing the database on errors.

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -1,17 +1,50 @@
 import { readFileSync } from 'node:fs'
 import { DatabaseSync } from 'node:sqlite'
 
+import { BaseError } from '../domains/error.ts'
+
+
+class InitializationError extends BaseError {
+	constructor(message: string, options?: ErrorOptions) {
+		super({
+			kind: 'ServerError',
+			message,
+			statusCode: 500,
+		}, options)
+	}
+}
+
+class DatabaseError extends BaseError {
+	constructor(message: string, options?: ErrorOptions) {
+		super({
+			kind: 'ServerError',
+			message,
+			statusCode: 500,
+		}, options)
+	}
+}
+
+
 /**
  * Initializes the database schema by executing the init.sql file.
  *
- * @param {DatabaseSync} database - The database instance to initialize.
+ * @param database - The database instance to initialize.
  */
 function initializeSchema(database: DatabaseSync): void {
-	const initScript = readFileSync(
-			new URL('./init.sql', import.meta.url),
-			'utf-8',
-	)
-	database.exec(initScript)
+	try {
+		const initScript = readFileSync(
+				new URL('./init.sql', import.meta.url),
+				'utf-8',
+		)
+		database.exec(initScript)
+	} catch (error) {
+		const message = 'Failed to initialize database schema: '
+		if (BaseError.isError(error)) {
+			throw new InitializationError(message.concat(error.message), {cause: error.cause})
+		} else {
+			throw new InitializationError(message.concat(String(error)))
+		}
+	}
 }
 
 /**
@@ -21,11 +54,25 @@ function initializeSchema(database: DatabaseSync): void {
  * @return A database instance with foreign keys enabled and schema initialized.
  */
 export function openDatabaseSync(path: URL): DatabaseSync {
-	const database = new DatabaseSync(path, {
-		enableForeignKeyConstraints: true,
-		open: false,
-	})
-	database.open()
-	initializeSchema(database)
-	return database
+	let database: undefined | DatabaseSync
+
+	try {
+		database = new DatabaseSync(path, {
+			enableForeignKeyConstraints: true,
+			open: false,
+		})
+		database.open()
+		initializeSchema(database)
+		return database
+	} catch (error) {
+		const message = `Failed to open database at ${path.pathname}: `
+		database?.isOpen && database.close()
+		if (error instanceof InitializationError) {
+			throw error
+		} else if (BaseError.isError(error)) {
+			throw new DatabaseError(message.concat(error.message), {cause: error.cause})
+		} else {
+			throw new DatabaseError(message.concat(String(error)))
+		}
+	}
 }
